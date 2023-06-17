99d63961b9ce6f26cc293a533b94f4818c426df9
Damien Sedgwick <damienksedgwick@gmail.com>
2023-06-17 21:23:17 +0100

    navigating between widget views is now working in demo app

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
-import React, { ReactNode, useState } from "react";
+import React, { Dispatch, ReactNode, SetStateAction, useState } from "react";
 import {
+  ArrowLeftIcon,
   ExclamationTriangleIcon,
   LightBulbIcon,
   StarIcon,
+  XMarkIcon,
 } from "@heroicons/react/24/outline";
 
 const widgetStyle = {
@@ -24,13 +26,10 @@ const widgetStyle = {
 };
 
 const widgetTitleStyle = {
-  position: "absolute" as const,
-  top: "10px",
-  left: "50%",
-  transform: "TranslateX(-50%)",
-  width: "100%",
-  fontSize: "1.25rem",
-  marginTop: "0",
+  fontSize: "1.15rem",
+  margin: "0",
+  paddingLeft: "1rem",
+  paddingRight: "1rem",
 };
 
 const widgetContentStyle = {
@@ -50,10 +49,37 @@ const widgetSelectionButtonStyle = {
   justifyContent: "space-between",
 };
 
-const FloopDefault = () => {
+type FloopDefaultProps = {
+  setShowWidget: Dispatch<SetStateAction<boolean>>;
+  setWidgetType: Dispatch<SetStateAction<WidgetType>>;
+};
+
+const FloopDefault = ({ setShowWidget, setWidgetType }: FloopDefaultProps) => {
   return (
     <div style={widgetContentStyle}>
-      <h1 style={widgetTitleStyle}>What would you like to say?</h1>
+      <div
+        style={{
+          position: "absolute" as const,
+          top: "10px",
+          left: "50%",
+          transform: "TranslateX(-50%)",
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <h1 style={widgetTitleStyle}>What would you like to say?</h1>
+        <XMarkIcon
+          width={20}
+          height={20}
+          onClick={() => setShowWidget(false)}
+          style={{
+            cursor: "pointer",
+            opacity: "0.5",
+          }}
+        />
+      </div>
       <div
         style={{
           display: "flex",
@@ -62,15 +88,24 @@ const FloopDefault = () => {
           marginTop: "2rem",
         }}
       >
-        <button style={widgetSelectionButtonStyle}>
+        <button
+          style={widgetSelectionButtonStyle}
+          onClick={() => setWidgetType("rating")}
+        >
           <StarIcon width={48} height={48} />
-          <small>rating</small>
+          <small>Rating</small>
         </button>
-        <button style={widgetSelectionButtonStyle}>
+        <button
+          style={widgetSelectionButtonStyle}
+          onClick={() => setWidgetType("issue")}
+        >
           <ExclamationTriangleIcon width={48} height={48} />
           <small>issue</small>
         </button>
-        <button style={widgetSelectionButtonStyle}>
+        <button
+          style={widgetSelectionButtonStyle}
+          onClick={() => setWidgetType("suggestion")}
+        >
           <LightBulbIcon width={48} height={48} />
           <small>suggestion</small>
         </button>
@@ -79,48 +114,114 @@ const FloopDefault = () => {
   );
 };
 
-const FloopRating = () => {
+const FloopRating = ({ setShowWidget, setWidgetType }: FloopDefaultProps) => {
   return (
     <div style={widgetContentStyle}>
-      <h1 style={widgetTitleStyle}>Rating</h1>
+      <div
+        style={{
+          position: "absolute" as const,
+          top: "10px",
+          left: "50%",
+          transform: "TranslateX(-50%)",
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <ArrowLeftIcon
+          width={20}
+          height={20}
+          onClick={() => setWidgetType("default")}
+          style={{ cursor: "pointer" }}
+        />
+        <h1 style={widgetTitleStyle}>Rate our product</h1>
+        <XMarkIcon
+          width={20}
+          height={20}
+          onClick={() => setShowWidget(false)}
+          style={{
+            cursor: "pointer",
+            opacity: "0.5",
+          }}
+        />
+      </div>
     </div>
   );
 };
 
-const FloopIssue = () => {
+const FloopIssue = ({ setShowWidget, setWidgetType }: FloopDefaultProps) => {
   return (
     <div style={widgetContentStyle}>
-      <h1 style={widgetTitleStyle}>Issue</h1>
+      <div
+        style={{
+          position: "absolute" as const,
+          top: "10px",
+          left: "50%",
+          transform: "TranslateX(-50%)",
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <ArrowLeftIcon
+          width={20}
+          height={20}
+          onClick={() => setWidgetType("default")}
+          style={{ cursor: "pointer" }}
+        />
+        <h1 style={widgetTitleStyle}>Report an issue</h1>
+        <XMarkIcon
+          width={20}
+          height={20}
+          onClick={() => setShowWidget(false)}
+          style={{
+            cursor: "pointer",
+            opacity: "0.5",
+          }}
+        />
+      </div>
     </div>
   );
 };
 
-const FloopSuggestion = () => {
+const FloopSuggestion = ({
+  setShowWidget,
+  setWidgetType,
+}: FloopDefaultProps) => {
   return (
     <div style={widgetContentStyle}>
-      <h1 style={widgetTitleStyle}>Suggestion</h1>
+      <div
+        style={{
+          position: "absolute" as const,
+          top: "10px",
+          left: "50%",
+          transform: "TranslateX(-50%)",
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <ArrowLeftIcon
+          width={20}
+          height={20}
+          onClick={() => setWidgetType("default")}
+          style={{ cursor: "pointer" }}
+        />
+        <h1 style={widgetTitleStyle}>Make a suggestion</h1>
+        <XMarkIcon
+          width={20}
+          height={20}
+          onClick={() => setShowWidget(false)}
+          style={{
+            cursor: "pointer",
+            opacity: "0.5",
+          }}
+        />
+      </div>
     </div>
-  );
-};
-
-const FloopWidgetFooter = () => {
-  return (
-    <small style={{ fontSize: "0.7rem", opacity: "0.5" }}>
-      Widget by{" "}
-      <span>
-        <strong>
-          <a
-            style={{
-              color: "inherit",
-            }}
-            href="https://www.feedback-loop.io"
-            target="_blank"
-          >
-            Floop
-          </a>
-        </strong>
-      </span>
-    </small>
   );
 };
 
@@ -129,33 +230,66 @@ type FloopWidgetProps = {
   children: ReactNode;
 };
 
+type WidgetType = "default" | "rating" | "issue" | "suggestion";
+
 export const FloopWidget = ({ projectId, children }: FloopWidgetProps) => {
   const [showWidget, setShowWidget] = useState(false);
-
-  const [widgetType, setWidgetType] = useState<
-    "default" | "rating" | "issue" | "suggestion"
-  >("default");
-
-  console.log(projectId);
+  const [widgetType, setWidgetType] = useState<WidgetType>("default");
 
   return (
     <div
       style={{
         position: "relative",
       }}
-      onClick={() => setShowWidget(!showWidget)}
-      role="button"
     >
-      {children}
+      <div onClick={() => setShowWidget(!showWidget)}>{children}</div>
 
       {showWidget ? (
         <div style={widgetStyle}>
-          {widgetType === "default" ? <FloopDefault /> : null}
-          {widgetType === "rating" ? <FloopRating /> : null}
-          {widgetType === "issue" ? <FloopIssue /> : null}
-          {widgetType === "suggestion" ? <FloopSuggestion /> : null}
+          {widgetType === "default" ? (
+            <FloopDefault
+              setWidgetType={setWidgetType}
+              setShowWidget={setShowWidget}
+            />
+          ) : null}
 
-          <FloopWidgetFooter />
+          {widgetType === "rating" ? (
+            <FloopRating
+              setWidgetType={setWidgetType}
+              setShowWidget={setShowWidget}
+            />
+          ) : null}
+
+          {widgetType === "issue" ? (
+            <FloopIssue
+              setWidgetType={setWidgetType}
+              setShowWidget={setShowWidget}
+            />
+          ) : null}
+
+          {widgetType === "suggestion" ? (
+            <FloopSuggestion
+              setWidgetType={setWidgetType}
+              setShowWidget={setShowWidget}
+            />
+          ) : null}
+
+          <small style={{ fontSize: "0.7rem", opacity: "0.5" }}>
+            Widget by{" "}
+            <span>
+              <strong>
+                <a
+                  style={{
+                    color: "inherit",
+                  }}
+                  href="https://www.feedback-loop.io"
+                  target="_blank"
+                >
+                  Floop
+                </a>
+              </strong>
+            </span>
+          </small>
         </div>
       ) : null}
     </div>

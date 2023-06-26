fdf855e7ce83a21c074529dc9cf7b6dc6228c8de
Damien Sedgwick <damienksedgwick@gmail.com>
2023-06-26 19:48:37 +0100

    changed font sizes

--- src/styles.ts
+++ src/styles.ts
@@ -17,6 +17,7 @@ export const widget = {
   fontFamily: "Inter, system-ui, Avenir, Helvetica, Arial, sans-serif",
   fontSize: "12px",
   fontWeight: "normal",
+  background: "white",
   boxShadow:
     "rgba(50, 50, 93, 0.25) 0 13px 27px -5px, rgba(0, 0, 0, 0.3) 0 8px 16px -8px",
   zIndex: "41189",
@@ -77,12 +78,16 @@ export const rating = {
   display: "flex",
   justifyContent: "space-between",
   boxSizing: "border-box",
+  fontSize: "12px",
+  fontWeight: "normal",
 } as CSSProperties;
 
 export const span = {
   display: "flex",
   alignItems: "center",
   gap: "6px",
+  fontSize: "12px",
+  fontWeight: "normal",
 } as CSSProperties;
 
 export const label = {
@@ -97,6 +102,8 @@ export const input = {
   borderRadius: "8px",
   boxSizing: "border-box",
   fontFamily: "Inter, system-ui, Avenir, Helvetica, Arial, sans-serif",
+  fontSize: "12px",
+  fontWeight: "normal",
 } as CSSProperties;
 
 export const textarea = {
@@ -107,6 +114,8 @@ export const textarea = {
   borderRadius: "8px",
   boxSizing: "border-box",
   fontFamily: "Inter, system-ui, Avenir, Helvetica, Arial, sans-serif",
+  fontSize: "12px",
+  fontWeight: "normal",
 } as CSSProperties;
 
 export const submit = {
@@ -118,17 +127,19 @@ export const submit = {
   boxShadow:
     "rgba(50, 50, 93, 0.15) 0 2px 4px -1px, rgba(0, 0, 0, 0.1) 0 1px 2px -1px",
   background: "none",
+  fontSize: "12px",
+  fontWeight: "normal",
 } as CSSProperties;
 
 export const footer = {
   textAlign: "center",
-  fontSize: "10px",
   color: "#94a3b8",
 } as CSSProperties;
 
 export const branding = {
   textAlign: "inherit",
   color: "#94a3b8",
+  fontSize: "10px",
   fontWeight: "bold",
   textDecoration: "none",
 } as CSSProperties;

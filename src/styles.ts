c635eb06bf3317f0a3d68b36ef959648574cac9c
Damien Sedgwick <damienksedgwick@gmail.com>
2023-08-27 16:17:14 +0100

    increased height by 20px

--- src/styles.ts
+++ src/styles.ts
@@ -9,7 +9,7 @@ export const trigger = {
 export const widget = {
   padding: "12px",
   width: "300px",
-  height: "200px",
+  height: "220px",
   borderRadius: "16px",
   boxShadow:
     "rgba(50, 50, 93, 0.25) 0 13px 27px -5px, rgba(0, 0, 0, 0.3) 0 8px 16px -8px",
